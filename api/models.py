ccea98928ec8f41a523d3ff6b585de049350f68a
msfernandes <matheus.souza.fernandes@gmail.com>
2015-06-01 23:28:21 -0300

    Fix create user API method
    Signed-off-by: msfernandes <matheus.souza.fernandes@gmail.com>

--- api/models.py
+++ api/models.py
@@ -31,9 +31,9 @@ class User(object):
 
     def __init__(self):
         self.date_joined = None
-        self.first_name = ''
-        self.email = ''
-        self.id = 0
+        self.first_name = None
+        self.email = None
+        self.id = None
         self.pontuation = 0
-        self.resource_uri = ''
-        self.username = ''
+        self.resource_uri = None
+        self.username = None
